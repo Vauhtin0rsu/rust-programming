05aee75f62ca9d18b66d93e58830ad5440a56026
Vauhtin0rsu <124376317+Vauhtin0rsu@users.noreply.github.com>
2025-08-01 11:19:24 +0300

    working version of the old mechanic

--- Project/src/main.rs
+++ Project/src/main.rs
@@ -1,25 +1,32 @@
 pub mod game;
-use game::Player::Player;
-use game::WeaponMap::WeaponMap;
-use game::MonsterMap::MonsterMap;
-use game::Weapon::Weapon;
 use game::Monster::Monster;
+use game::MonsterMap::MonsterMap;
+use game::Player::Player;
+use game::Weapon::Weapon;
+use game::WeaponMap::WeaponMap;
 
-use std::io;
 use rand::Rng;
+use std::io;
 
 fn menu(name: &str, function: &str) -> i8 {
+    // Different menus are printed depending on what "function" is called (could have a better name)
     if function == "main" {
         println!("\nMitä tehdään, {}?", name);
-        println!("1) Seikkaile tyrmissä     2) Käy kaupassa     3) Hakkaa lohikäärme     0) Luovuta");
+        println!("1) Seikkaile tyrmissä     2) Käy kaupassa     3) Hakkaa lohikäärme    4) Tarkista pelaajana attribuutit     0) Luovuta");
     }
 
     if function == "battle_normal" {
-        println!("1) Hyökkää    2) Puolustaudu     3) Juo lääkerohto    4) Juokse pakoon");
+        println!("\n1) Hyökkää    2) Puolustaudu     3) Juo lääkerohto    4) Juokse pakoon");
+    }
+
+    if function == "battle_boss" {
+        println!("\n1) Hyökkää    2) Väistä hyökkäystä    3) Erityishyökkäys    4) Juo lääkerohto");
     }
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Virhe syötteen lukemisessa");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Virhe syötteen lukemisessa");
     let input = input.trim();
 
     let i: i8 = match input.trim().parse() {
@@ -27,14 +34,18 @@ fn menu(name: &str, function: &str) -> i8 {
         Err(_) => {
             println!("Virhe syötteen lukemisessa");
             -1
-        } 
+        }
     };
     i
 }
 
-fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
-    // pick a weapon for the dungeon
-    println!("Minkä aseen aiot ottaa mukaan?");
+fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster>) {
+    println!("\nLähestyt kohti tyrmää...");
+
+    let mut boss_multiplier: f32 = 1.0;
+    boss_multiplier += 0.1 * player.bosses_won as f32; // make enemies stronger depending on how many bosses the player has beaten
+
+    println!("Minkä aseen aiot ottaa mukaan?"); // pick a weapon for the dungeon
     let mut i: i32 = 1;
     for weapon in &player.weapons {
         println!("{}) {}", i, weapon.name);
@@ -42,7 +53,9 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
     }
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Virhe syötteen lukemisessa");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Virhe syötteen lukemisessa");
     let input = input.trim();
 
     let i: usize = match input.trim().parse() {
@@ -50,7 +63,7 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
         Err(_) => {
             println!("Virhe syötteen lukemisessa");
             return;
-        } 
+        }
     };
 
     if i > player.weapons.len() {
@@ -58,26 +71,29 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
         return;
     }
 
-    let curnt_weapon: &Weapon = &player.weapons[i-1];
+    let curnt_weapon: &Weapon = &player.weapons[i - 1];
     println!("{}! Hyvä valinta!", curnt_weapon.name);
-    
-    // loop through the monster fights
-    println!("Lähestyt kohti tyrmää...");
-    for monster in monsters {
+
+    for monster in monsters {   // loop through the monster fights
+        monster.hp = monster.hp + 5 * player.bosses_won; // give monsters more hope depending on the difficulty level
+
         if player.curnt_hp < 1 {
             break;
         }
+
         println!("\nEteesi hyökkää {}!", monster.name);
         println!("Elämäpisteet: {}", monster.hp);
         if curnt_weapon.element == monster.weakness {
             println!("Aseesi toimii erityisen hyvin tähän hirviöön!");
         }
 
-        loop {
+        loop {         
             let j = menu("","battle_normal");
+            let mut player_defence_multiplier: f32 = 1.0;
             match j {
-                1 => {
-                    let mut player_dmg_float: f32 = rand::rng().random_range(15..25) as f32 * player.str;
+                1 => { // normal attack, players and weapons strength affect
+                    let mut player_dmg_float: f32 =
+                        rand::rng().random_range(15..25) as f32 * player.str;
                     if curnt_weapon.element == monster.weakness {
                         player_dmg_float = player_dmg_float * curnt_weapon.str;
                     }
@@ -87,19 +103,22 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
                     println!("Teet vahinkoa {} elämäpistettä", player_dmg_int);
 
                     if monster.hp < 1 {
-                        println!("Voitit hirviön!! Saat siitä hyvästä {} kultaa", monster.gold);
+                        println!(
+                            "Voitit hirviön!! Saat siitä hyvästä {} kultaa",
+                            monster.gold
+                        );
                         player.gold += monster.gold;
-                        // append_monsterfile(monster.name);
                         break;
                     }
-                } 
-                2 => {
-
                 }
-                3 => {
+                2 => { // Possibility to defence, this multiplier is effective when the monster attacks
+                    println!("Valmistaudut ottamaan hyökkäyksen vastaan");
+                    player_defence_multiplier = rand::rng().random_range(0.45..0.8);
+                }
+                3 => { // drinking a potion
                     if player.potions > 0 {
                         println!("Juot lääkerohdon");
-                        player.curnt_hp += rand::rng().random_range(30..40);
+                        player.curnt_hp += rand::rng().random_range(40..56);
                         player.potions -= 1;
 
                         if player.curnt_hp > player.max_hp {
@@ -108,14 +127,12 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
 
                         println!("Elämäpisteet: {}", player.curnt_hp);
                         println!("Lääkerohtoja jäljellä: {}", player.potions);
-                        
-
                     } else {
                         println!("Lääkerohtoja ei ole jäljellä!");
                         continue;
                     }
                 }
-                4 => {
+                4 => { // Running away from the monster (made easy)
                     println!("Pakenet hirviön kynsistä");
                     break;
                 }
@@ -123,13 +140,16 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
                     continue;
                 }
             }
-            
-            println!("\n{} hyökkää päällesi!!", monster.name);
-            let monster_dmg_float: f32 = rand::rng().random_range(10..20) as f32 * monster.str;
+
+            println!("\n{} hyökkää päällesi!!", monster.name); // Monster attacks, monsters strength, players defence and bosses won affect
+            let monster_dmg_float: f32 = rand::rng().random_range(10..20) as f32 * monster.str * player_defence_multiplier * boss_multiplier;
             let monster_dmg_int: i32 = monster_dmg_float.round() as i32;
             player.curnt_hp -= monster_dmg_int;
 
-            println!("{} tekee vahinkoa {} elämäpistettä", monster.name, monster_dmg_int);
+            println!(
+                "{} tekee vahinkoa {} elämäpistettä",
+                monster.name, monster_dmg_int
+            );
             println!("\nElämäpisteitä jäljellä: {}", player.curnt_hp);
 
             if player.curnt_hp < 1 {
@@ -140,12 +160,137 @@ fn battle_normal(player: &mut Player, monsters: &mut Vec<Monster> ) {
     }
 }
 
-fn battle_boss(boss: &Monster, player: &mut Player, wm: &WeaponMap) {
-    
+fn battle_boss(monster: &Monster, player: &mut Player, wm: &WeaponMap) { 
+    // This is very similar to the normal battle funcion
+    player.curnt_hp = player.max_hp; // full hp for the boss :)
+    let mut boss: Monster = monster.clone();
+    println!("\nLähestyt urheasti kohti paikkaa, jossa {} on viimeksi nähty...", boss.name);
+
+    println!("Minkä aseen aiot ottaa mukaan?");
+    let mut i: i32 = 1;
+    for weapon in &player.weapons {
+        println!("{}) {}", i, weapon.name);
+        i += 1;
+    }
+
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Virhe syötteen lukemisessa");
+    let input = input.trim();
+
+    let i: usize = match input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => {
+            println!("Virhe syötteen lukemisessa");
+            return;
+        }
+    };
+
+    if i > player.weapons.len() {
+        println!("Virhe syötteen lukemisessa, palataan päävalikkoon.");
+        return;
+    }
+
+    let mut curnt_weapon: Weapon = player.weapons[i - 1].clone();
+    println!("{}! Hyvä valinta!", curnt_weapon.name);
+    if curnt_weapon.element == boss.weakness {
+        println!("Aseesi tekee ekstravahinkoa lohikäärmeeseen!");
+    }
+
+    let mut round_counter: i32 = 0; // Counter because the boss attacks only on every third round
+    loop {
+        let j: i8 = menu("","battle_boss");
+        round_counter += 1;
+        match j {
+            1 => {
+                let mut player_dmg_float: f32 =
+                    rand::rng().random_range(15..25) as f32 * player.str;
+                if curnt_weapon.element == boss.weakness {
+                    player_dmg_float = player_dmg_float * curnt_weapon.str;
+                }
+
+                let player_dmg_int: i32 = player_dmg_float.round() as i32;
+                boss.hp -= player_dmg_int;
+                println!("Teet vahinkoa {} elämäpistettä", player_dmg_int);
+
+            }
+            2 => { // This completely skips the round (could be something else tho)
+                println!("Väistät mahdollisen hyökkäyksen varalta!");
+                continue;
+            }
+            3 => {
+                if curnt_weapon.special_attacks > 0 {
+                    println!("Teet erityishyökkäyksen!");
+                    
+                    let mut player_dmg_float: f32 = rand::rng().random_range(50..80) as f32 * player.str;
+                    player_dmg_float = player_dmg_float * curnt_weapon.str;
+
+                    let player_dmg_int: i32 = player_dmg_float.round() as i32;
+                    boss.hp -= player_dmg_int;
+                    println!("Teet vahinkoa {} elämäpistettä", player_dmg_int);   
+                    curnt_weapon.special_attacks -= 1;          
+
+                } else {
+                    println!("Tällä aseella ei ole erityishyökkäystä tai olet tehnyt niitä liikaa!");
+                    round_counter -= 1;
+                    continue;
+                }
+            }
+            4 => {
+                if player.potions > 0 {
+                    println!("Juot lääkerohdon");
+                    player.curnt_hp += rand::rng().random_range(45..56);
+                    player.potions -= 1;
+
+                    if player.curnt_hp > player.max_hp {
+                        player.curnt_hp = player.max_hp;
+                    }
+
+                    println!("Elämäpisteet: {}", player.curnt_hp);
+                    println!("Lääkerohtoja jäljellä: {}", player.potions);
+                } else {
+                    println!("Lääkerohtoja ei ole jäljellä!");
+                    continue;
+                }                
+            }
+            _ => {
+                round_counter -= 1;
+                continue;
+            }
+        }
+        if boss.hp <= 0 {
+            println!("Olet voittanut lohikäärmeen! Hurraa!!!");
+            // append_monsterfile
+            player.max_hp += 50;
+            player.str += 0.33;
+            player.weapons.push(wm.legendary[player.bosses_won as usize].clone());
+            println!("Lohikäärmeen voitettuasi se jättää sinulle uuden harvinaisen aseen nimeltään {}", wm.legendary[player.bosses_won as usize].name);
+            player.bosses_won += 1;
+            break; 
+        }
+
+        if round_counter % 3 == 0 {
+            println!("\n{} hyökkää sinua kohti!!", boss.name);
+            let boss_dmg_float: f32 = rand::rng().random_range(40..65) as f32 * boss.str;
+            let boss_dmg_int: i32 = boss_dmg_float.round() as i32;
+            player.curnt_hp -= boss_dmg_int;
+
+            println!("{} tekee vahinkoa {} elämäpistettä", boss.name, boss_dmg_int);
+            println!("\nElämäpisteitä jäljellä: {}", player.curnt_hp);
+
+            if player.curnt_hp < 1 {
+                println!("{} on voittanut sinut!", boss.name);
+                break;
+            }
+        } else {
+            println!("{} valmistautuu vielä hyökkäykseensä...", boss.name);
+        }
+    }
 }
 
 fn weapon_market(player: &mut Player, wm: &WeaponMap) {
-    println!("Myynnissä olevat aseet:");
+    println!("\nMyynnissä olevat aseet:");
 
     let mut i: i32 = 1;
     for weapon in &wm.for_the_shop {
@@ -155,7 +300,9 @@ fn weapon_market(player: &mut Player, wm: &WeaponMap) {
     println!("0) Ei kiitos");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Virhe syötteen lukemisessa");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Virhe syötteen lukemisessa");
     let input = input.trim();
 
     let i: usize = match input.trim().parse() {
@@ -163,7 +310,7 @@ fn weapon_market(player: &mut Player, wm: &WeaponMap) {
         Err(_) => {
             println!("Virhe syötteen lukemisessa");
             return;
-        } 
+        }
     };
 
     if i > wm.for_the_shop.len() {
@@ -176,26 +323,30 @@ fn weapon_market(player: &mut Player, wm: &WeaponMap) {
         return;
     }
 
-    if player.gold >= wm.for_the_shop[i-1].cost {
-        println!("\n{}! Saamanne pitää", wm.for_the_shop[i-1].name);
-        player.gold -= wm.for_the_shop[i-1].cost;
-        player.weapons.push(wm.for_the_shop[i-1].clone());
+    if player.gold >= wm.for_the_shop[i - 1].cost {
+        println!("\n{}! Saamanne pitää", wm.for_the_shop[i - 1].name);
+        player.gold -= wm.for_the_shop[i - 1].cost;
+        player.weapons.push(wm.for_the_shop[i - 1].clone());
     } else {
         println!("Valitettavasti sinulla ei ole varaa tähän aseeseen.");
     }
-} 
+}
 
 fn potion_market(player: &mut Player) {
     println!("\nHaluatko ostaa lääkerohtoja? (k / e)");
 
     let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Virhe syötteen lukemisessa");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Virhe syötteen lukemisessa");
 
     if input.trim() == "k" {
         println!("Kuinka monta? Yksi lääkerohto on 20 kultaa.");
 
         let mut input = String::new();
-        io::stdin().read_line(&mut input).expect("Virhe syötteen lukemisessa");
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Virhe syötteen lukemisessa");
         let i: usize = match input.trim().parse() {
             Ok(num) => num,
             Err(_) => {
@@ -209,23 +360,20 @@ fn potion_market(player: &mut Player) {
             player.gold -= sum;
             player.potions += i as i32;
             println!("Ostit juuri {} lääkerohtoa", i as i32);
+        } else {
+            println!("Rahasi eivät riitä!");
+            potion_market(player);
         }
     }
 }
 
-// fn append_monsterfile(name: &str) {
-
-// }
-
-// fn analyze_monsterfile() {
-
-// }
-
 fn main() {
-   println!("Hei seikkailija! Mikä on nimesi?");
+    println!("Hei seikkailija! Mikä on nimesi?");
 
     let mut name = String::new();
-    io::stdin().read_line(&mut name).expect("Virhe syötteen lukemisessa");
+    io::stdin()
+        .read_line(&mut name)
+        .expect("Virhe syötteen lukemisessa");
     let name = name.trim();
 
     let mut player: Player = Player::new();
@@ -234,10 +382,9 @@ fn main() {
 
     // Pick the starting weapon, delete the rest
     let mut rng_max: usize = wm.starters.len();
-    println!("{}", rng_max);
     let starting_weapon: Weapon = wm.starters[rand::rng().random_range(0..rng_max)].clone();
     println!("Hurraa! Ensimmäinen aseesi on {}", starting_weapon.name);
-    
+
     player.weapons.push(starting_weapon);
     wm.starters = vec![];
 
@@ -252,22 +399,28 @@ fn main() {
                 let mut dungeon_monsters: Vec<Monster> = vec![];
                 rng_max = mm.normals.len() - 1;
                 for mut _j in 0..3 {
-                    let rng_monster: Monster = mm.normals[rand::rng().random_range(0..rng_max)].clone();
+                    let rng_monster: Monster =
+                        mm.normals[rand::rng().random_range(0..rng_max)].clone();
                     dungeon_monsters.push(rng_monster);
                 }
-                battle_normal(&mut player, &mut dungeon_monsters);        
-
+                battle_normal(&mut player, &mut dungeon_monsters);
             }
-            2  => {
+            2 => {
+                println!("\nLöydät tiesi ostoksille");
                 println!("Sinulla on {} kultaa.", player.gold);
                 weapon_market(&mut player, &wm);
                 potion_market(&mut player);
             }
             3 => {
                 battle_boss(&mm.bosses[player.bosses_won as usize], &mut player, &wm);
-
             }
-
+            4 => {
+                println!("\nPelaajan {} tämänhetkiset attribuutit:", name);
+                println!("Elämäpisteet: {}", player.max_hp);
+                println!("Rahat: {} kultaa", player.gold);
+                println!("Voimakerroin: {}", player.str);
+                println!("Lohikäärmeitä voitettu: {}", player.bosses_won);
+            }
             0 => {
                 break;
             }
@@ -275,6 +428,9 @@ fn main() {
                 continue;
             }
         }
+        if player.bosses_won == 3 {
+            println!("Hurraa! Olet voittanut pelin! Olet mestari!!!! Jee!!!!!");
+            break;
+        }
     }
-    //analyze_monsterfile()
 }
